4bc8c3190383895c38dde397f0c1227706804a96
TesrSK <davidkoscak1@gmail.com>
2024-11-18 19:24:08 +0100

    LeafNode + tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,9 +1,10 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
+    # HTMLNode
     def test_to_html_props(self):
         node = HTMLNode("div", "Hello, world!", None, {"class": "hero", "href": "google.com"})
         self.assertEqual(node.props_to_html(),' class="hero" href="google.com"')
@@ -23,7 +24,22 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("div", "The div is surely centered", None, {"class": "center"})
 
         self.assertEqual(node.__repr__(), "HTMLNode(div, The div is surely centered, children: None, {'class': 'center'})")
-    
+
+    # LeaNode
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "This is the value.")
+
+        self.assertEqual(node.to_html(), "<p>This is the value.</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "No tags just raw text.")
+
+        self.assertEqual(node.to_html(), "No tags just raw text.")
+
+    def test_repr_LeafNode(self):
+        node = LeafNode("div", "The div is surely centered", {"class": "center"})
+
+        self.assertEqual(node.__repr__(), "LeafNode(div, The div is surely centered, {'class': 'center'})")
 
 
 if __name__ == "__main__":
